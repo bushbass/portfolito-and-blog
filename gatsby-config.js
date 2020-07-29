476544944f6665e532ef9ee97eba8a2bc863015d
Alex Nielsen <alex@bigal.org>
2020-07-28 21:41:10 -0400

    from camp

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,16 +2,16 @@ module.exports = {
   siteMetadata: {
     title: `Alex Nielsen's Portfolio!`,
     siteUrl: `https://www.AlexNielsen.com`,
-    description: `Blog and Portfolio for Alex Nielsen Web Developer`
+    description: `Blog and Portfolio for Alex Nielsen Web Developer`,
   },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
-        path: `${__dirname}/src/`
-      }
+        path: `${__dirname}/src/`,
+      },
     },
-    `gatsby-transformer-remark`
-  ]
+    `gatsby-transformer-remark`,
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,31 @@
 import React from "react"
-import Layout from "../components/layout"
+import Header from "../components/Header"
 
 export default function Home() {
   return (
-    <Layout>
-      <h1>
-        Home page for Alex Nielsen
-        <img src={"/alex-pink-backround-pattern.png"} alt="" />
-      </h1>
-    </Layout>
+    <main>
+      <Header />
+      <h1 class="name">Alex Nielsen</h1>
+      <h2 class="name title">Web Developer</h2>
+      <h3>HTML / CSS / Javascript / React </h3>
+      <p>
+        make contact easy, put social or contact in main and footer or maybe
+        make social media icons fixed when navigating the page
+      </p>
+      <div>
+        <img
+          class="my-image"
+          src="http://mtnielsen.com/wp-content/uploads/2018/09/me-mets-tiles-web-square-300x300.jpg"
+        />
+      </div>
+      <p>Skills section goes here</p>
+
+      <div>
+        <p>
+          Making a return to tech after taking some time to care for my
+          children. I am currently seeking a full-time web developer role.
+        </p>
+      </div>
+    </main>
   )
 }
