4ea8927527d2b84fcff9c536b5587fe54af2a96f
Alex Nielsen <alex@bigal.org>
2020-10-19 11:24:16 -0400

    updated resume page to new netlify redirect

--- src/pages/old-resume-link.js
+++ src/pages/old-resume-link.js
@@ -6,7 +6,9 @@ export default function Resume() {
     <Layout>
       <div>
         Current resume is on{" "}
-        <a href="http://AlexNielsen.com">AlexNielsen.com</a>
+        <a href="https://docs.google.com/document/d/18eDLRzIFup-lC8mWi8nNW9tGP2T2E4MWowMOrh8KK38/edit">
+          AlexNielsen.com
+        </a>
       </div>
     </Layout>
   )
